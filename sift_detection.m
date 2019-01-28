f89d91dc2e0324da46c272b17ad994bfd6849e12
Margaux <margaux@metadot.com>
2019-01-28 00:05:24 -0800

    Generates coordinates such as [(x,y),(x,y)...] in the .txt

--- sift_detection.m
+++ sift_detection.m
@@ -1,17 +1,17 @@
-I = imread('NotreDame5.jpg') ;
+I = imread('NotreDame1.jpg') ;
 
 figure(1);
 image(I) ;
 I = single(rgb2gray(I)) ;
 [f,d] = vl_sift(I) ; 
-% f is a feature frame and has the format [X;Y;S;TH], 
+% f is a feature frame and has the format [X;Y;S;TH],
 % where X,Y is the (fractional) center of the frame, 
 % S is the scale and TH is the orientation (in radians)
 
 length = size(f,2);
 % we take only the 200 last keypoints because
 % they are the highest response keypoints (highest scale)
-last_200_points = length-200:length;
+last_200_points = length-200+1:length;
 highest_response_keypoints = f(:,last_200_points);
 
 h1 = vl_plotframe(highest_response_keypoints);
@@ -25,9 +25,11 @@ figure(2);
 plot(x,-y,'o');
 
 
+
 A = [x; y];
-fileID = fopen('results/NotreDame5.txt','w');
-fprintf(fileID,'%6s %12s\n','x','y');
-fprintf(fileID,'%6.2f %12.8f\n',A);
+fileID = fopen('NotreDame1.txt','w');
+fprintf(fileID,'%6s %12s','[');
+fprintf(fileID,'(%6.2f, %12.8f),', A);
+fprintf(fileID,'%6s %12s',']');
 fclose(fileID);
 
